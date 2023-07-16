e5c9fe2ea3d1cf960243e5f3ce92b5d9c457f662
Roman <r.ammeleon@gmail.com>
2023-07-16 19:06:40 +0300

    Added tests on pyTest

---
+++ TestCalculatorPyTest.py
@@ -0,0 +1,21 @@
+from Calculator import Calculator
+
+
+def test_add():
+    c = Calculator()
+    assert c.add(1, 2) == 3
+
+
+def test_subtract():
+    c = Calculator()
+    assert c.subtract(1, 2) == -1
+
+
+def test_multiply():
+    c = Calculator()
+    assert c.multiply(1, 2) == 2
+
+
+def test_divide():
+    c = Calculator()
+    assert c.divide(1, 2) == 0.5
